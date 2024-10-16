0581bb398aed41727048bb3f7e9e8d19acd1d775
Sayf-allah Gabsi <sgabsi@paul-f5Br6s7.clusters.42paris.fr>
2024-10-16 12:10:10 +0200

    correction strdup

--- srcs/ft_str/ft_strdup.c
+++ srcs/ft_str/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: sgabsi <sgabsi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 14:57:46 by sgabsi            #+#    #+#             */
-/*   Updated: 2024/09/10 17:24:49 by sgabsi           ###   ########.fr       */
+/*   Updated: 2024/10/16 12:09:42 by sgabsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strdup(const char *s1)
 	size_t	len;
 	char	*s2;
 
-	if (!s1 || !*s1)
+	if (!s1)
 		return (NULL);
 	len = ft_strlen(s1);
 	s2 = (char *)ft_calloc(len + 1, sizeof (char));
@@ -31,7 +31,7 @@ char	*ft_strndup(const char *s1, size_t n)
 	size_t	len;
 	char	*s2;
 
-	if (!s1 || !*s1)
+	if (!s1)
 		return (NULL);
 	len = ft_strlen(s1);
 	if (len > n)

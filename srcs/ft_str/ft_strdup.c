f747d35b5e2e4b8b890893b3122bdbaeef003eab
Sayf-allah Gabsi <sgabsi@paul-f5Br6s7.clusters.42paris.fr>
2024-10-16 12:14:08 +0200

    better version of strdup : use of strndup to strdup

--- srcs/ft_str/ft_strdup.c
+++ srcs/ft_str/ft_strdup.c
@@ -6,25 +6,11 @@
 /*   By: sgabsi <sgabsi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 14:57:46 by sgabsi            #+#    #+#             */
-/*   Updated: 2024/10/16 12:09:42 by sgabsi           ###   ########.fr       */
+/*   Updated: 2024/10/16 12:12:58 by sgabsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-char	*ft_strdup(const char *s1)
-{
-	size_t	len;
-	char	*s2;
-
-	if (!s1)
-		return (NULL);
-	len = ft_strlen(s1);
-	s2 = (char *)ft_calloc(len + 1, sizeof (char));
-	if (!s2)
-		return (NULL);
-	return ((char *)ft_memmove(s2, s1, len));
-}
 
 char	*ft_strndup(const char *s1, size_t n)
 {
@@ -40,4 +26,9 @@ char	*ft_strndup(const char *s1, size_t n)
 	if (!s2)
 		return (NULL);
 	return ((char *)ft_memmove(s2, s1, len));
+}
+
+char	*ft_strdup(const char *s1)
+{
+	return (ft_strndup(s1, ft_strlen(s1)));
 }
